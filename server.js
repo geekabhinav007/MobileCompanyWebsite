76325a0188bb41a6499159b57d743744369cfdaf
geekabhinav007 <abhinav26january@gmail.com>
2023-03-07 13:25:27 +0530

    intialized project at local

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost:27017/mobile-store', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+// Start the server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
