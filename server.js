61b888f265aaa88d021aab20817320e952926c15
geekabhinav007 <abhinav26january@gmail.com>
2023-03-07 16:46:12 +0530

    changed the login setting

--- server.js
+++ server.js
@@ -35,72 +35,22 @@ const userSchema = new mongoose.Schema({
 const Contact = mongoose.model('Contact', contactSchema);
 const User = mongoose.model('User', userSchema);
 
-// Route to handle login submissions
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
 
-  // Find a user with the matching username and password in the database
-  const user = await User.findOne({ username, password });
+// Route to handle form submissions
+app.post('/submit', async (req, res) => {
+  const { name, email, message } = req.body;
 
-  if (user) {
-    res.send('Login successful');
-  } else {
-    res.status(401).send('Invalid username or password');
+  // Create a new contact object using the Contact model
+  const newContact = new Contact({ name, email, message });
+
+  try {
+    // Save the new contact object to the database
+    await newContact.save();
+    res.send('Form submitted successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
   }
-});
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  // Here you can check the username and password against your database
-  // and authenticate the user
-
-  // For example, you can check if the user exists in your database:
-  User.findOne({ username: username }, (err, user) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('An error occurred');
-    }
-
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-
-    // If the user is found, check if the password is correct
-    if (user.password === password) {
-      // Authentication successful, redirect to the user's dashboard
-      res.redirect('/dashboard');
-    } else {
-      // Incorrect password, show an error message
-      res.status(401).send('Incorrect password');
-    }
-  });
-});
-
-// Middleware function to check if user is authenticated
-const requireAuth = (req, res, next) => {
-  if (!req.session.isAuthenticated) {
-    return res.redirect('/login');
-  }
-  next();
-};
-
-// Set up routes
-app.get('/login', (req, res) => {
-  res.render('login'); // Replace 'login' with the name of your login page
-});
-
-app.post('/login', (req, res) => {
-  // Authenticate user and set req.session.isAuthenticated to true if successful
-  // Redirect to dashboard or some other authenticated page if successful
-});
-
-app.get('/index', requireAuth, (req, res) => {
-  res.render('index'); 
-});
-
-app.get('/', (req, res) => {
-  res.redirect('/login');
 });
 
 
